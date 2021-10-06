a41b87d0e4470a7151f7a894bc2b61a72ade6e37
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-06 18:40:15 +0200

    Add files via upload

--- TP3/src/couleur_compteur.c
+++ TP3/src/couleur_compteur.c
@@ -4,25 +4,25 @@
 #include <time.h>
 
 
-struct Couleurs {
-    __u_char rouge; //__u_ pour protéger les variables
+struct Couleurs { //création de la structure couleur
+    char rouge;  
 
-    __u_char vert;
+    char vert;
 
-    __u_char bleu;
+    char bleu;
 
-    __u_char alpha;
+    char alpha;
 
 };
 
 int main() {
-    struct Couleurs tableau[10];
+    struct Couleurs tableau[100]; 
 
     srand(time(NULL));
 
-    for( int i = 0 ; i < 97 ; i++) {
+    for (int i = 0; i < 97; i++) { //création du tableau de 100 couleurs aléatoires (on en crée ici 97 pour pouvoir tester le programme avec 3 couleurs similaires)
     
-        int rouge = rand();
+        int rouge = rand(); //on assigne des numéros aléatoires pour chaque couleur
         int vert = rand();
         int bleu = rand();
         int alpha = rand();
@@ -33,6 +33,7 @@ int main() {
         tableau[i].alpha = alpha;
 
     }
+
     tableau[97].rouge = 0xaa;
     tableau[97].vert = 0xaa;
     tableau[97].bleu = 0xaa;
@@ -49,24 +50,26 @@ int main() {
     tableau[99].alpha = 0xaa;
 
     
-    int cpt = 1;
+    int cpt = 1; //compteur du nombre d'occurence de chaque couleur
     
-    for (int i = 0; i < 100; i++) {
-        for (int j = i+1; j < 100; j++) {
-            if ((tableau[i].rouge == tableau[j].rouge) && (tableau[i].vert == tableau[j].vert) && (tableau[i].bleu == tableau[j].bleu) && (tableau[i].alpha == tableau[j].alpha)) {
-                sup(tableau.rouge,j);
-                sup(tableau.vert,j);
-                sup(tableau.bleu,j);
-                sup(tableau.alpha,j);
-                cpt ++;
+    for (int i = 0; i < 100; i++) { //on parcourt une première fois la liste des couleurs
+        for (int j = i+1; j < 100; j++) { //on la parcourt une deuxième fois pour comparer chaque couleur du tableau
+            if ((tableau[i].rouge == tableau[j].rouge) && (tableau[i].vert == tableau[j].vert) && (tableau[i].bleu == tableau[j].bleu) && (tableau[i].alpha == tableau[j].alpha)) { //si deux couleurs dans le tableau sont similaires
+                tableau[j].rouge = -1; //on remplace dans le tableau une des couleurs pour ne plus la prendre en compte ensuite
+                tableau[j].vert = -1;
+                tableau[j].bleu = -1;
+                tableau[j].alpha = -1;
+                cpt ++; //on incrémente le compteur des occurences 
             }
         }
-        if (i != 99) {
-            printf("%02x %02x %02x %02x, %d \n", tableau[i].rouge, tableau[i].vert, tableau[i].bleu, tableau[i].alpha, cpt);
+        
+        if ((tableau[i].rouge != -1) && (tableau[i].vert != -1) && (tableau[i].bleu != -1) && (tableau[i].alpha != -1)) { //si on est sur une couleur qui doit être traitée (et non un doublon)
+            printf("%02x %02x %02x %02x, %d \n", tableau[i].rouge, tableau[i].vert, tableau[i].bleu, tableau[i].alpha, cpt); //on affiche la couleur en question avec son nombre d'occurence dans le tableau
+
         }
-        cpt = 1;
-    }  
-    
+        cpt = 1; //on réinitialise le compteur à 1 puis on repart dans les deux boucles for
+    }
+          
     return(0);
 }
  
