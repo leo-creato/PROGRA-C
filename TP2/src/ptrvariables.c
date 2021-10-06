e0e3e8a757b7ade4d95838668f5c862b5c92f5d9
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-06 19:52:04 +0200

    Add files via upload

---
+++ TP2/src/ptrvariables.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    //création de toutes les variables et de leurs pointeurs associés
+    char my_char_var;
+    char* cptr = &my_char_var;
+
+    short my_short_var = -12;
+    short* sptr = &my_short_var;
+
+    int my_int_var = 12;
+    int* iptr = &my_int_var;
+
+    long my_long_var = -1234553L;
+    long* lptr = &my_long_var;
+
+    long long my_llong_var = 1123345LL;
+    long long* llptr = &my_llong_var;
+
+    float my_float_var = 3.14;
+    float* fptr = &my_float_var;
+
+    double my_double_var = 3.14E-12;
+    double* dptr = &my_double_var;
+
+    long double my_long_double_var = 3.14E-22;
+    long double* ldptr = &my_long_double_var;
+
+    //affectation
+    *cptr = 'a';
+    *sptr = -12;
+    *iptr = 12;
+    *lptr =  -1234553L;
+    *llptr = 1123345LL;
+    *fptr = 3.14;
+    *dptr = 3.14E-12;
+    *ldptr = 3.14E-22;
+
+    //affichage
+    printf("%c\n", *cptr);
+    printf("%hd\n", *sptr);
+    printf("%d\n", *iptr);
+    printf("%ld\n", *lptr);
+    printf("%lld\n", *llptr);
+    printf("%f\n", *fptr);
+    printf("%g\n", *dptr);
+    printf("%Lg\n", *ldptr);
+
+    return(0);
+}
---
+++ TP2/src/tableauptr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    int tab_int[10]; //création du tableau de 10 entiers
+    int* iptr = (int *)&tab_int; //création du pointeur sur la première valeur du tableau d'entier
+    
+
+    float tab_float[10]; //création du tableau de 10 nombres décimaux
+    float* fptr = (float *)&tab_float; //création du pointeur sur la première valeur du tableau de nombres décimaux
+
+    for (int i = 0; i < 10; i++) { //on remplit aléatoirement les 2 tableaux
+        tab_int[i] = rand();
+        tab_float[i] = ((float)rand()/(float)RAND_MAX) * 1000; //création des nombres décimaux aléatoires
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (i % 2 == 0){ //si l'indice est divisible par 2
+            *(iptr+i) *= 3; //on accède à l'entier pointé par le pointeur qu'on multiplie par 3
+            *(fptr+i) *= 3; //on accède au nombre décimal pointé par le pointeur qu'on multiplie par 3
+        }
+    }
+
+    //on affiche les deux tableaux après modification
+    printf("Tableau d'entiers modifié: \n");
+    for (int i = 0; i < 10; i++){printf("%d\n",tab_int[i]);} 
+
+    printf("Tableau des nombres décimaux modifié: \n");
+    for (int i = 0; i < 10; i++){printf("%f\n",tab_float[i]);}
+
+    return(0);
+}
